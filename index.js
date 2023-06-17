af956e8e1df81592067089454885fd119f366b98
jv699 <jvacarch@gmail.com>
2023-06-16 20:20:55 -0400

    index.html - added about me
    index.css
    - added paragraph style
    
    index.js
    - added method to populate my age

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // scripts come after where they're used    
+const CUR_DATE = new Date();
+const BIRTH_DATE = new Date(99,10,6);
+
+const findAge = ((currentdate, birthDate) => {
+    var diffTime = Math.abs(currentdate - birthDate);
+    var diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return Math.floor(diffDays / 365);
+});
 
 const changeText = (thingToChange, textToChange) => {
     document.getElementById(thingToChange).innerHTML = textToChange;
@@ -10,6 +18,8 @@ const changeColor = (elementId, color) => {
 
 const button = document.getElementById('hornyButton');
 const headings = document.getElementById('headings');
-// headings.style.color = 'red';
+
+const ageSpan = document.getElementById('ageSpan');
+ageSpan.innerHTML = `${findAge(CUR_DATE, BIRTH_DATE)} year old `;
 
 button.onclick = () => changeColor(headings.id, 'red');
